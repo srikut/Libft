603ebaef4ef7a2b3a5506f7704f6a830120bb91c
srikuto <codemaster.neo.net@gmail.com>
2024-11-26 16:48:38 +0900

    kansei

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,16 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: srikuto <srikuto@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: sometani <sometani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 18:35:26 by srikuto           #+#    #+#             */
-/*   Updated: 2024/11/06 20:48:13 by srikuto          ###   ########.fr       */
+/*   Updated: 2024/11/25 10:54:53 by sometani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-char *ft_strtrim(char const *s1, char const *set)
+size_t ft_strlen(const char *s)
 {
-	
+    size_t len = 0;
+    while (s[len])
+        len++;
+    return len;
+}
+int ft_is_in_set(char c, const char *set)
+{
+    while (*set)
+    {
+        if (*set == c)
+            return 1;
+        set++;
+    }
+    return 0;
+}
+char *ft_strtrim(const char *s1, const char *set)
+{
+    size_t start = 0;
+    size_t end;
+    char *trimmed;
+    size_t i;
+
+    while (!s1 || !set)
+        return NULL;
+    end = ft_strlen(s1);
+    while (s1[start] && ft_is_in_set(s1[start], set))
+        start++;
+    while (end > start && ft_is_in_set(s1[end - 1], set))
+        end--;
+    trimmed = (char *)malloc(sizeof(char) * (end - start + 1));
+    while (!trimmed)
+        return NULL;
+    i = 0;
+    while (start < end)
+    {
+        trimmed[i] = s1[start];
+        i++;
+        start++;
+    }
+	trimmed[i] = '\0';
+    return trimmed;
 }
